dde51770b48cde2be5b6cdc614dc4ef70004083c
ericgroh <grohec.1995@gmail.com>
2017-03-04 13:34:58 -0500

    nav button focused

--- public/js/index.js
+++ public/js/index.js
@@ -35,6 +35,19 @@ $(window).scroll(
   }
 );
 
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+});
+
 
 // var header = $('#header').offset().top,
 // var about = $('#about').offset().top,
